3cb36bb848305538a5caa048783426262caf3e40
DataSciGenius <datascigenius@gmail.com>
2024-02-05 00:04:28 +0100

    first code, help and comments were studied.

--- introduction_to_python.py
+++ introduction_to_python.py
@@ -1,4 +1,27 @@
-# First python output with 'Print' functions
+# region first code
+# let's write to console "Hello world!"
 print("Hello world!")
-print("Hi, Python!")
+# you can run the code with different manners.
+# hit Ctrl + enter when you end of the line
+# hit Ctrl + enter when you select the whole code
+# let's try some examples
 print("Hello AI Era!")
+print("Hi Python!")
+# endregion
+
+# region help
+# Python help function is used to display the documentation of modules, functions, classes, keywords, etc.
+help(print)
+# When you clicl "Ctrl + mouse, then you can open the documentation
+
+# endregion
+
+# region comment
+# this is a commetnand to write a comment use "#" symbol
+print("Hello world!") # this code prints "Hello world!"
+# print "Hello"
+print("Hello")
+
+"""This is a multiline comment
+You can write comments whenever you want."""
+# endregion
